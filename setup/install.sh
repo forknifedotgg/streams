0b0c971fdf47c02e7e68389857e3d5cb6be86cb8
Jeremi Gendron <forknifedotgg@gmail.com>
2019-04-20 16:42:51 +0000

    save progress, start working on server

--- setup/install.sh
+++ setup/install.sh
@@ -4,7 +4,13 @@ sudo apt-get install build-essential autoconf ffmpeg zlib1g-dev libssl-dev libpc
 mkdir data && cd data
 git clone https://github.com/nginx/nginx
 git clone https://github.com/arut/nginx-rtmp-mobule
-cd nginx && ./auto/configure --add-module=/home/ubuntu/data/nginx-rtmp-module
+cd nginx && ./auto/configure --add-module=../nginx-rtmp-module
 sudo make install -j$(nproc)
-sudo cp ../nginx/nginx.conf /usr/local/nginx/conf/nginx.conf
-
+sudo cp ../../nginx/nginx.conf /usr/local/nginx/conf/nginx.conf
+sudo /usr/local/nginx/sbin/nginx
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+export NVM_DIR="${XDG_CONFIG_HOME/:-$HOME/.}nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install 11
+cd ../../server
+npm install
