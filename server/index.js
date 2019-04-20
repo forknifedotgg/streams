70f649257f730514bdd68151de629f44c6d8f0d6
Jeremi Gendron <forknifedotgg@gmail.com>
2019-04-20 18:59:31 +0000

    save progress on server index.js

--- server/index.js
+++ server/index.js
@@ -15,34 +15,85 @@ express.post('/:app/:stream_key', async (req, res) => {
 
   sessions[stream_key] = req
 
-  fs.createWriteStream('./cache/' + stream_key)
+  const cacheStreamWrite = fs.createWriteStream('./cache/' + stream_key)
+  cacheStreamWrite.on('error', (error) => {
+    console.log('cacheStreamRead error: ', error)
+  })
+  req.pipe(cacheStreamWrite)
 
-  const valid = await verifyStreamKey(stream_key)
-  if (!valid) {
+  const error = await verifyStreamKey(stream_key)
+  if (error) {
     fs.unlink('./cache/' + stream_key)
     invalid[stream_key] = true
-    logInvalidAttempt(req)
+    logInvalidAttempt(req, error)
     res.end()
     if (sessions[stream_key]) delete sessions[stream_key]
+    return
   }
 
-  let data = ''
-  const probe = ffprobe(stream_key)
-  probe.stderr.on('data', chunk => data += chunk.toString())
-  probe
+  let probeStats = ''
+  const probe = ffprobe()
+  probe.stderr.on('data', chunk => probeState += chunk.toString())
+  probe.stderr.on('end', () => {
+    const { data, error } = verifyProbeStats(probeStats)
+    if (error) {
+      fs.unlink('./cache/' + stream_key)
+      logInvalidAttempt(req, error)
+      res.end()
+      if (sessions[stream_key]) delete sessions[stream_key]
+    }
+    else {
+      const outStream = await getWriteStream(req, data)
+      cacheStreamWrite.on('unpipe', (src) => {
+        req.pipe(outStream)
+      })
+      logStreamStart(req, data)
+    }
+  })
+  const cacheStreamRead = fs.createReadStream('./cache/' + stream_key)
+  cacheStreamRead.on('error', (error) => {
+    console.log('cacheStreamRead error: ', error)
+  })
+  cacheStreamRead.pipe(probe.stdin)
+
 })
 
-function ffprobe(stream_key) {
+
+async function getWriteStream(req, data) {}
+
+function ffprobe() {
   return spawn('ffprobe', ['-i', 'pipe:0', '-hide_banner', '-read_intervals', '"%+2"'])
 }
 
+function verifyProbeStats(probeStats) {
+  return {
+    error: null,
+    data: {
+      video: {
+        resolution: '',
+        fps: '',
+        bitrate: '',
+      },
+      audio: {
+        samplerate: '',
+        channels: '',
+        bitrate: ''
+      }
+    }
+  }
+}
+
 async function verifyStreamKey(stream_key) {
-  return true
+  return false
   // verify user table, whether banned or if out of hours etc.
 }
 
-async function logInvalidAttempt(req) {
+async function logInvalidAttempt(req, data) {
   // record request information for stream with invalid key
+}
+
+async function logStreamStart(req, data) {
+
 }
 
 express.listen(10000)
